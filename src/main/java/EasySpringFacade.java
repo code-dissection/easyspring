f95eeb7bf9a50281b0dedd17e414162eef358e3d
Ruslan Sitdikov <rsitdiko1@yandex.ru>
2026-06-04 15:19:28 +0400

    Add framework start point.

---
+++ src/main/java/EasySpringFacade.java
@@ -0,0 +1,15 @@
+import bootstrap.ContextPipeline;
+
+public class EasySpringFacade {
+
+    private EasySpringFacade() {
+    }
+
+    public static void run(Class<?> mainClass) {
+        ContextPipeline contextPipeline = new ContextPipeline();
+
+        String packageToScan = mainClass.getPackageName();
+
+        contextPipeline.process(packageToScan);
+    }
+}
--- src/main/java/bootstrap/ContextPipeline.java
+++ src/main/java/bootstrap/ContextPipeline.java
@@ -9,10 +9,8 @@ import java.util.Map;
 public class ContextPipeline {
 
     private final PipelineWorker pipelineWorker = new PipelineWorker();
-    //todo get value
-    private String packageToScan;
 
-    public void start(String packageToScan) {
+    public void process(String packageToScan) {
         //Phase 1. Scan classpath for annotated classes
         List<ClassPropertiesContainer> classInfos = pipelineWorker.scanAppCode(packageToScan);
 
